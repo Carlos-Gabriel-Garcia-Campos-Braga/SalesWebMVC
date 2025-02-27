c1f277218d053173d51ead7be7491fc1e5f4bacb
Carlos Gabriel Garcia Campos <carlosgabrielbraga035@gmail.com>
2025-02-27 17:38:54 -0300

    Second DataBase Update and new Entities

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,8 +1,37 @@
-﻿namespace SalesWebMVC.Models
+﻿using Microsoft.AspNetCore.Mvc.Formatters.Internal;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Seller Seller)
+        {
+            Sellers.Add(Seller);
+        }
+
+        public double TotalSales(DateTime Initial, DateTime Final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(Initial, Final));
+        }
     }
+
+    
 }
+
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double BaseSalary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> SalesRecord { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Department = department;
+        }
+
+        public void AddSale(SalesRecord SaleRecord)
+        {
+            SalesRecord.Add(SaleRecord);
+        }
+
+        public void RemoveSale(SalesRecord SaleRecord)
+        {
+            SalesRecord.Remove(SaleRecord);
+        }
+
+        public double TotalSales(DateTime Initial, DateTime Final)
+        {
+            return SalesRecord.
+                Where(sr => sr.Date >= Initial && sr.Date <= Final).
+                Sum(sr => sr.Amount);
+        }
+    }
+}
