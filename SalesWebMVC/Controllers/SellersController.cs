4de6778dcac23a8eacd2868750dc91ec324aaf7d
Carlos Gabriel Garcia Campos <carlosgabrielbraga035@gmail.com>
2025-02-28 15:36:32 -0300

    Update Seller GET and POST

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -2,6 +2,9 @@
 using SalesWebMVC.Models;
 using SalesWebMVC.Models.ViewModels;
 using SalesWebMVC.Services;
+using SalesWebMVC.Services.Exceptions;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace SalesWebMVC.Controllers
@@ -78,5 +81,49 @@ namespace SalesWebMVC.Controllers
 
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _SellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _DepartmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _SellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(NotFoundException)
+            {
+                return NotFound();
+            }
+            catch(DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
+
+
     }
 }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Services.Exceptions;
 
 namespace SalesWebMVC.Services
 {
@@ -22,7 +23,7 @@ namespace SalesWebMVC.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
-            _context.SaveChanges();
+            _context.SaveChanges(); 
         }
 
         public Seller FindById(int id)
@@ -35,6 +36,25 @@ namespace SalesWebMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("ID not found!");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+            
         }
     }
 }
