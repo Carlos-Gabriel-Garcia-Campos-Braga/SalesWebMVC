b39eca721844a69655601c1e20127947bec6062a
Carlos Gabriel Garcia Campos <carlosgabrielbraga035@gmail.com>
2025-02-28 09:56:59 -0300

    SellerService

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
+using System.Threading;
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _SellerService;
+        public SellersController(SellerService sellerService)
+        {
+            _SellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _SellerService.FindAll();
+            return View(list);
         }
     }
 }
